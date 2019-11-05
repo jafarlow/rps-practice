98e5e35b3ac2fdbd1f6ab1cf8f5545c5dbee2658
jafarlow <jacobafarlow@gmail.com>
2019-11-05 11:17:07 -0500

    Added outcome messaging and style
    index.html
    - shifted the battle to the bottom of the page
    - added an h3 element inside .battle to display the ai choice message
    - added an h2 element beneath the .battle for the user message
    
    index.scss
    - removed margin from h3
    - removed height from .battle
    - added flex-direction and align status to .battle
    - added margin setting for .options
    - added win/lose/draw coloring to .selected
    
    events.js
    - established messageElement and aiMessageElement and targeted the
    matching HTML id
    - added aiMessage via innerHTML
    - established win/lose/draw message text
    - targeted messageElement within the conditional statement and used
    innerHTML to set the appropriate message
    - replaced the console logs with appropriate classList.add conditions

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -2,6 +2,8 @@ let options = document.getElementsByClassName("option")
 let choices = ["rock", "paper", "scissors"]
 let winState = { rock: "scissors", paper: "rock", scissors: "paper"}
 let battleElement = document.getElementById("battle")
+let aiMessageElement = document.getElementById("ai-message")
+let messageElement = document.getElementById("message")
 
 for (let i = 0; i < options.length; i++) {
   let option = options[i]
@@ -31,12 +33,7 @@ const displayChoices = function (player, ai) {
   let choiceElement = document.createElement("div")
   choiceElement.classList.add("ai-choice", ai)
 
-  let header = document.createElement("h3")
-  let text = document.createTextNode("AI chooses: ")
-  header.classList.add("h3")
-  header.appendChild(text)
-
-  battleElement.appendChild(header)
+  aiMessageElement.innerHTML = "AI chooses:"
   battleElement.appendChild(choiceElement)
 }
 
@@ -46,14 +43,19 @@ const battle = function (option) {
 
   // AI
   let aiChoice = choices[random(2, 0)]
-
+  let draw = "It's a draw!"
+  let win = "Player wins!"
+  let lose = "AI wins!"
 
   if (choice === aiChoice) {
-    console.log("Draw!");
+    option.classList.add("draw")
+    messageElement.innerHTML = draw
   } else if (aiChoice === winState[choice]) {
-    console.log("Player wins!");
+    option.classList.add("winner")
+    messageElement.innerHTML = win
   } else {
-    console.log("AI wins!");
+    option.classList.add("loser")
+    messageElement.innerHTML = lose
   }
   displayChoices(choice, aiChoice)
 }
