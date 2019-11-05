f2e1cd0dcaf7c0edf0a6da324afd6ac050bcafea
jafarlow <jacobafarlow@gmail.com>
2019-11-05 10:08:12 -0500

    Expanded battle function
    index.html
    - Added data-choice as attribute to RPS
    
    events.js
    - Established winState
    - Established the choices array
    - inserted "this" to battle call within click event
    - Created randomizer function to return an integer. In this case the max
    and min values will be small, as choices.length == 3
    - declared user choice
    - declared AI choice
    - Established placeholder conditional statement utilizing winState and
    console.log

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,4 +1,6 @@
 let options = document.getElementsByClassName("option")
+let choices = ["rock", "paper", "scissors"]
+let winState = { rock: "scissors", paper: "rock", scissors: "paper"}
 
 for (let i = 0; i < options.length; i++) {
   let option = options[i]
@@ -6,7 +8,7 @@ for (let i = 0; i < options.length; i++) {
   option.addEventListener("click", function () {
     this.classList.add("selected")
     disableOptions()
-    battle()
+    battle(this)
   })
 }
 
@@ -20,6 +22,22 @@ const disableOptions = function () {
   }
 }
 
-const battle = function () {
+const random = function (max, min) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
+const battle = function (option) {
+  // user
+  let choice = option.dataset.choice
+
+  // AI
+  let aiChoice = choices[random(2, 0)]
+
+  if (choice === aiChoice) {
+    console.log("Draw!");
+  } else if (aiChoice === winState[choice]) {
+    console.log("Player wins!");
+  } else {
+    console.log("AI wins!");
+  }
 }
